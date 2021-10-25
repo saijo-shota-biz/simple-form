39026b7ce769a34deb43c4ac348bb3b3d23c9777
saijo.shota.biz <saijo.shota.biz@gmail.com>
2021-10-25 11:22:07 +0900

    init

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import theme from '../theme/theme';
-import '../config/firebaseConfig';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
